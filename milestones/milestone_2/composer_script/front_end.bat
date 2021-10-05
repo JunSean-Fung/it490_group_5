6d64dca776c57b69cc78022f20a08709fb9ccec3
JunSean-Fung <46977322+JunSean-Fung@users.noreply.github.com>
2021-10-05 11:30:43 -0400

    Update front_end.bat

--- milestones/milestone_2/composer_script/front_end.bat
+++ milestones/milestone_2/composer_script/front_end.bat
@@ -1,1 +1,14 @@
-net start apache2.4
+@echo off
+title Apache 2.4
+:: Check for apache 2.4 status
+set server = \\25.51.181.62
+for /f "tokens=4" %%a in ('sc %server% query apache2.4 ^| findstr STATE') do set _CmdResult=%%a
+:: Check value
+if %_CmdResult%==STOPPED (
+    echo Attempting to start Apache 2.4 
+    net start apache2.4
+) else (
+    echo apache 2.4 is %_CmdResult% 
+)
+
+pause
