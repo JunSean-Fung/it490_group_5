5bbb41cdc52b9275104a3c1f339e337f7b9a718a
sa977 <50147823+sa977@users.noreply.github.com>
2021-10-10 19:40:56 -0400

    Update database.sh

--- milestones/milestone_2/composer_script/database.sh
+++ milestones/milestone_2/composer_script/database.sh
@@ -2,8 +2,9 @@ set +v echo off
 
 #Status Check
 status = $(systemctl check mysql)
-if [$status == "inactive"] then
-	echo "MySQL is" $status, "attempting to start MySQL"
+if [ "$status" = "inactive" ]; 
+then
+	echo "MySQL is" $status", attempting to start MySQL"
 	sudo systemctl start mysql
 else
 	echo -e "MySQL is" $status "\n\n"
