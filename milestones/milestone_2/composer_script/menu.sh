7a242caaf1b2101d03f5194a7bdfaa5d6cddf1eb
JunSean-Fung <46977322+JunSean-Fung@users.noreply.github.com>
2021-10-10 17:57:15 -0400

    Update menu.sh

--- milestones/milestone_2/composer_script/menu.sh
+++ milestones/milestone_2/composer_script/menu.sh
@@ -1,16 +1,16 @@
-echo "welcome please select your options"
+echo "Control menu"
 read choice
 apache=$(sudo systemctl check apache2)
 startapache=`sudo systemctl start apache2`
 
 case $choice in
-         1) status of apache is $apache
+         1) echo status of apache is $apache
                           ;;
          2) echo stopping Apache2
             sudo systemctl stop apache2
                           ;;
          3) echo Starting Apache2
-            sudo systemctl start apach2
+            sudo systemctl start apache2
                           ;;
          *) echo exit
 esac
