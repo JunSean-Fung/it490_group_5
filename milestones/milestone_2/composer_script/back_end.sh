7457e8eb1eb9213bb864f3d2612f0dc461a2d2af
JunSean-Fung <46977322+JunSean-Fung@users.noreply.github.com>
2021-10-10 15:50:09 -0400

    Update back_end.sh

--- milestones/milestone_2/composer_script/back_end.sh
+++ milestones/milestone_2/composer_script/back_end.sh
@@ -4,15 +4,15 @@ set +vecho off
 
 # Front End Server
 echo Attempting to Ping Front-End server...
-ping 25.51.181.62
+ping 25.83.212.229
 echo ===================================================================
 # Messaging Server
 echo Attempting to Ping Messaging server...
-ping 25.51.161.242
+ping 25.74.57.122
 echo ===================================================================
 # Database Server
 echo Attempting to Ping Database Server...
-
+ping 25.79.46.137
 echo ===================================================================
 
 echo ===========================Ping End================================
