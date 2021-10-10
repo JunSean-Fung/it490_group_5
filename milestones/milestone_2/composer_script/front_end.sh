792caa1e25e8666d988d26f551004dc5723824f4
Zachary <zacharymanatee@gmail.com>
2021-10-10 18:49:49 -0400

    Fixed start for front end composer script

--- milestones/milestone_2/composer_script/front_end.sh
+++ milestones/milestone_2/composer_script/front_end.sh
@@ -4,7 +4,7 @@ set +v echo off
 status=$(systemctl check apache2)
 if [ "$status" = "inactive" ]; then
     echo attempting to start apache2
-    sudo systemctl stop apache2 
+    sudo systemctl start apache2 
 else
     echo $status
 fi
