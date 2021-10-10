7c34d77c4ac2c28d88ba8f37b75e1bd727b34dc9
JunSean-Fung <46977322+JunSean-Fung@users.noreply.github.com>
2021-10-10 18:32:22 -0400

    Update front_end.sh

--- milestones/milestone_2/composer_script/front_end.sh
+++ milestones/milestone_2/composer_script/front_end.sh
@@ -2,9 +2,12 @@ set +v echo off
 
 # Check for apache 2.4 status
 status=$(systemctl check apache2)
+
 offStatus="inactive"
 if [ $status == $offStatus ]
 then
-    echo Apache is currently off, attempting to start Apache
+    echo "Apache is" $status ", attempting to start Apache"
     sudo systemctl start apache2 
+else
+    echo -e "Apache is" $status "\n\n"
 fi
