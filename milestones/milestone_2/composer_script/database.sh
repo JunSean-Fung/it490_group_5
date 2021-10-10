67a4c05d2a6af68da139f7cf53686eb03df533aa
sa977 <50147823+sa977@users.noreply.github.com>
2021-10-10 19:04:21 -0400

    Update database.sh

--- milestones/milestone_2/composer_script/database.sh
+++ milestones/milestone_2/composer_script/database.sh
@@ -1,16 +1,13 @@
-set +v
-title mySQL80
-#Set to run as admin
-if ! "$1"="am_admin" (powershell start -verb runas '$0' am_admin & exit-b)
+set +v echo off
 
-# Check for apache 2.4 status
-export server = //25.51.181.62
-for -f "tokens=4" a in ['sc $server query mySQL80 ^| findstr STATE']; do set _CmdResult=a
-# Check value
-if $_CmdResult=STOPPED (
-    echo Attempting to start mySQL80
-    net start mySQL80
-) else (
-    echo mySQL80 is $_CmdResult 
-)
-pause
+#Status Check
+status = $(ssh ip address systemctl check mysql)
+
+offStatus = "inactive"
+if [$status == $offStatus]
+then
+	echo "MySQL is" $status, "attempting to start MySQL"
+	sudo systemctl start mysql
+else
+	echo -e "MySQL is" $status "\n\n"
+fi
