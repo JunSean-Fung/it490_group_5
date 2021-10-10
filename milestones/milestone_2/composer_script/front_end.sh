e8ebb085016450f76d0b7dfa4c968e9f71d1ece8
zac v <zjv2@njit.edu>
2021-10-10 19:14:42 -0400

    Updated front end composer script

--- milestones/milestone_2/composer_script/front_end.sh
+++ milestones/milestone_2/composer_script/front_end.sh
@@ -2,21 +2,10 @@ set +v echo off
 
 # Check for apache 2.4 status
 status=$(systemctl check apache2)
-<<<<<<< HEAD
 if [ "$status" = "inactive" ]; then
-    echo attempting to start apache2
-    sudo systemctl start apache2 
-else
-    echo $status
-fi
-=======
-
-offStatus="inactive"
-if [ $status == $offStatus ]
-then
     echo "Apache is" $status", attempting to start Apache"
     sudo systemctl start apache2 
 else
     echo -e "Apache is" $status "\n\n"
 fi
->>>>>>> 54bf84305f04cfce58fc3a1262f6e74b49d6ae42
+
