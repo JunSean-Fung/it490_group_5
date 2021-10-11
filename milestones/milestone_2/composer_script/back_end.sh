4f227253d1792193bb92d3ba6d3dc18304a3b761
JunSean-Fung <46977322+JunSean-Fung@users.noreply.github.com>
2021-10-10 20:57:53 -0400

    Update back_end.sh

--- milestones/milestone_2/composer_script/back_end.sh
+++ milestones/milestone_2/composer_script/back_end.sh
@@ -12,13 +12,14 @@ do
     #ssh debian@25.83.212.229 systemctl check apache2
     echo ===================================================================
     # Messaging Server
-    echo Attempting to Ping Messaging server...
+    #echo Attempting to Ping Messaging server...
     ping -c 4 25.74.57.122
     ssh rabbit@25.74.57.122 systemctl check rabbitmq-server
     echo ===================================================================
     # Database Server
     echo Attempting to Ping Database Server...
     ping -c 4 25.79.46.137
+    ssh samer@25.79.46.137 systemctl check mysql
     echo ===================================================================
 
     echo ===========================Ping End================================
