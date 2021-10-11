f9db1e4d609666ac6071365a82a40fe481da52a2
JunSean-Fung <46977322+JunSean-Fung@users.noreply.github.com>
2021-10-10 20:03:45 -0400

    update

--- milestones/milestone_2/composer_script/back_end.sh
+++ milestones/milestone_2/composer_script/back_end.sh
@@ -1,21 +1,27 @@
 set +vecho off
 
 # Continuous loop for system check
+a=1
+b=1
 
-# Front End Server
-echo Attempting to Ping Front-End server...
-ping -c 4 25.83.212.229
-echo ===================================================================
-# Messaging Server
-echo Attempting to Ping Messaging server...
-ping -c 4 25.74.57.122
-echo ===================================================================
-# Database Server
-echo Attempting to Ping Database Server...
-ping -c 4 25.79.46.137
-echo ===================================================================
+while [ $a -eq $b ]
+do
+    # Front End Server
+    echo Attempting to Ping Front-End server...
+    ping -c 4 25.83.212.229
+    #ssh debian@25.83.212.229 systemctl check apache2
+    echo ===================================================================
+    # Messaging Server
+    echo Attempting to Ping Messaging server...
+    ping -c 4 25.74.57.122
+    ssh rabbit@25.74.57.122 systemctl check rabbitmq-server
+    echo ===================================================================
+    # Database Server
+    echo Attempting to Ping Database Server...
+    ping -c 4 25.79.46.137
+    echo ===================================================================
 
-echo ===========================Ping End================================
-echo ===================================================================
-
+    echo ===========================Ping End================================
+    echo ===================================================================
+done
 
