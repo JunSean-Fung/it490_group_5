2351b0ed00136b431bd5c7ef256043b3dbef03b2
JunSean-Fung <46977322+JunSean-Fung@users.noreply.github.com>
2021-10-10 22:02:31 -0400

    Create sshAddKey.sh

---
+++ milestones/milestone_2/composer_script/sshAddKey.sh
@@ -0,0 +1,7 @@
+ssh-copy-id -f -i backend_server.pub rabbit@25.74.57.122
+
+ssh-copy-id -f -i backend_server.pub junseanfung@25.80.87.100
+
+ssh-copy-id -f -i backend_server.pub paul@25.4.8.61
+
+ssh-copy-id -f -i backend_server.pub Samer@25.79.46.137
