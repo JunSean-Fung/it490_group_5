03556bd250d0cedc8df04f1370158f59d252a793
JunSean-Fung <46977322+JunSean-Fung@users.noreply.github.com>
2021-10-10 21:03:19 -0400

    Update back_end.sh

--- milestones/milestone_2/composer_script/back_end.sh
+++ milestones/milestone_2/composer_script/back_end.sh
@@ -7,18 +7,21 @@ b=1
 while [ $a -eq $b ]
 do
     # Front End Server
-    echo Attempting to Ping Front-End server...
-    ping -c 4 25.83.212.229
+    #echo Attempting to Ping Front-End server...
+    #ping -c 4 25.83.212.229
+    echo Attempting to check front-end service
     #ssh debian@25.83.212.229 systemctl check apache2
     echo ===================================================================
     # Messaging Server
-    #echo Attempting to Ping Messaging server...
+    echo Attempting to Ping Messaging server...
     ping -c 4 25.74.57.122
+    echo Attempting to check messaging service
     ssh rabbit@25.74.57.122 systemctl check rabbitmq-server
     echo ===================================================================
     # Database Server
     echo Attempting to Ping Database Server...
     ping -c 4 25.79.46.137
+    echo Attempting to check database service
     ssh samer@25.79.46.137 systemctl check mysql
     echo ===================================================================
 
