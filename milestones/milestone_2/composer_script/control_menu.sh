3946b2ce1e2bf50519e11f62953bbb102bfd6b76
JunSean-Fung <46977322+JunSean-Fung@users.noreply.github.com>
2021-10-12 19:25:27 -0400

    Update control_menu.sh

--- milestones/milestone_2/composer_script/control_menu.sh
+++ milestones/milestone_2/composer_script/control_menu.sh
@@ -9,27 +9,33 @@ do
     echo " 3: Turn on all services"
     echo " 4: Turn off all services"
     echo " 4: Turn off all services"
+    echo " z: Debug"
     echo " CTR-C can also exit the control menu"
     echo "=============Options======================"
     
     read option
     # hosts 
-    frontEnd=junseanfung@25.80.87.100
+    frontEnd=paul@25.4.8.61
+    message=rabbit@25.74.57.122
+    database=samer@25.79.46.137
+    debugTest=junseanfung@25.80.87.100
     case $option in
             1)  echo -e "Checking all services activity:\n"
                 echo -e "Apache is: "
-                ssh paul@25.4.8.61 systemctl check apache2
+                ssh $frontEnd systemctl check apache2
                 echo -e "\nRabbitmq is: "
-                ssh rabbit@25.74.57.122 systemctl check rabbitmq-server
+                ssh $message systemctl check rabbitmq-server
                 echo -e "\nmySQL is: "
-                ssh samer@25.79.46.137 systemctl check mysql
+                ssh $database systemctl check mysql
                             ;;
-            2)  ssh $frontEnd systemctl check apache2
+            2)  ssh $frontEnd systemctl status apache2
                             ;;
             3)  echo -e "Starting apache server\n\n"
                 sudo systemctl start apache2
                 echo -e "Stopping Apache server\n\n"
                 sudo systemctl stop apache2
+                            ;;
+            z)  
                             ;;
             *)  echo exit
     esac
