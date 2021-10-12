d74fbc8913498cceb60c99a6329e8cf4ff116f4f
JunSean-Fung <46977322+JunSean-Fung@users.noreply.github.com>
2021-10-12 14:55:46 -0400

    Update chmod.sh

--- milestones/milestone_2/composer_script/chmod.sh
+++ milestones/milestone_2/composer_script/chmod.sh
@@ -1,13 +1,5 @@
 # This file gives permission access to the file
 
-# Give execute permission
-#chmod +x ./back_end.sh
-#chmod +x ./front_end.sh
-#chmod +x ./menu.sh
-#chmod +x ./database.sh
-#chmod +x ./messaging.sh
-#chmod +x ./sshAddKey.sh
-#chmod +x ./git.sh
-
-find /home/junseanfung/it490_group_5/milestones/milestone_2/composer_script -type f -iname "*.sh" -exec chmod +x {} \;
-find /home/junseanfung/it490_group_5/milestones/milestone_2/composer_script -type f -iname "*.py" -exec chmod +x {} \;
+# Give execute permission to all files based on the format
+find -type f -iname "*.sh" -exec chmod +x {} \;
+find -type f -iname "*.py" -exec chmod +x {} \;
