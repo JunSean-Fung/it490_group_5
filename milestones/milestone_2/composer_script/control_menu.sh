c14cf41096e23d520ba96a3ec494609253b9f4b0
JunSean-Fung <46977322+JunSean-Fung@users.noreply.github.com>
2021-10-12 23:48:41 -0400

    update

--- milestones/milestone_2/composer_script/control_menu.sh
+++ milestones/milestone_2/composer_script/control_menu.sh
@@ -1,11 +1,6 @@
 # Control Menu script
 source ./system.sh
-
-# host + ip
-frontEnd=paul@25.4.8.61
-message=rabbit@25.74.57.122
-database=samer@25.79.46.137
-debugTest=junseanfung@25.80.87.100
+soruce ./servers.sh
 
 run (){
     echo ""
---
+++ milestones/milestone_2/composer_script/servers.sh
@@ -0,0 +1,5 @@
+# host + ip
+frontEnd=paul@25.4.8.61
+message=rabbit@25.74.57.122
+database=samer@25.79.46.137
+debugTest=junseanfung@25.80.87.100
