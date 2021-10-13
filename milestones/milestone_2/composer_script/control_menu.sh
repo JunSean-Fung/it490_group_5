7557587951efac7d9e6bad182b4f0dd73648802b
JunSean-Fung <46977322+JunSean-Fung@users.noreply.github.com>
2021-10-12 23:31:03 -0400

    Update control_menu.sh

--- milestones/milestone_2/composer_script/control_menu.sh
+++ milestones/milestone_2/composer_script/control_menu.sh
@@ -1,5 +1,11 @@
 # Control Menu script
 
+# host + ip
+frontEnd=paul@25.4.8.61
+message=rabbit@25.74.57.122
+database=samer@25.79.46.137
+debugTest=junseanfung@25.80.87.100
+
 lineSeperate(){
     printf '=%.0s' {1..25}
     msg=${1:-=}
@@ -122,68 +128,64 @@ turnOffOneService(){
             *)  echo exit
     esac
 }
-# host + ip
-frontEnd=paul@25.4.8.61
-message=rabbit@25.74.57.122
-database=samer@25.79.46.137
-debugTest=junseanfung@25.80.87.100
+run (){
+echo ""
+showOptions
+read option
+case $option in
+        1)  echo -e "Checking all services activity:\n"
+            echo -e "Apache: "
+            checkActive $frontEnd apache2
+            echo -e "\nRabbitmq: "
+            checkActive $message rabbitmq
+            echo -e "\nmySQL: "
+            checkActive $database mysql
+            echo -e "\ndebug apache: "
+            checkActive $debugTest apache2
+                        ;;
+        2)  echo -e "Checking all services status(detailed):\n"
+            echo -e "Apache: "
+            checkStatus $frontEnd apache2
+            echo -e "\nRabbitmq: "
+            checkStatus $message rabbitmq
+            echo -e "\nmySQL: "
+            checkStatus $database mysql
+            echo -e "\ndebug apache: "
+            checkStatus $debugTest apache2
+                        ;;
+        3)  echo -e "Turning on all services:\n"
+            echo -e "Apache: "
+            turnOnService $frontEnd apache2
+            echo -e "\nRabbitmq: "
+            turnOnService $message rabbitmq
+            echo -e "\nmySQL: "
+            turnOnService $database mysql
+            echo -e "\ndebug apache: "
+            turnOnService $debugTest apache2
+                        ;;
+        4)  echo -e "Turning off all services:\n"
+            echo -e "Apache: "
+            turnOffService $frontEnd apache2
+            echo -e "\nRabbitmq: "
+            turnOffService $message rabbitmq
+            echo -e "\nmySQL: "
+            turnOffService $database mysql
+            echo -e "\ndebug apache: "
+            turnOffService $debugTest apache2
+                        ;;
+        5)  turnOnOneService
+                        ;;
+        6)  turnOffOneService
+                        ;;
+        z)  checkActive $debugTest apache2
+                        ;;
+        *)  echo exit
+esac
+}
 
-# A endless loop until Ctl-C
 a=1
 b=1
 while [ $a -eq $b ]
 do
-    echo ""
-    showOptions
-    read option
-    case $option in
-            1)  echo -e "Checking all services activity:\n"
-                echo -e "Apache: "
-                checkActive $frontEnd apache2
-                echo -e "\nRabbitmq: "
-                checkActive $message rabbitmq
-                echo -e "\nmySQL: "
-                checkActive $database mysql
-                echo -e "\ndebug apache: "
-                checkActive $debugTest apache2
-                            ;;
-            2)  echo -e "Checking all services status(detailed):\n"
-                echo -e "Apache: "
-                checkStatus $frontEnd apache2
-                echo -e "\nRabbitmq: "
-                checkStatus $message rabbitmq
-                echo -e "\nmySQL: "
-                checkStatus $database mysql
-                echo -e "\ndebug apache: "
-                checkStatus $debugTest apache2
-                            ;;
-            3)  echo -e "Turning on all services:\n"
-                echo -e "Apache: "
-                turnOnService $frontEnd apache2
-                echo -e "\nRabbitmq: "
-                turnOnService $message rabbitmq
-                echo -e "\nmySQL: "
-                turnOnService $database mysql
-                echo -e "\ndebug apache: "
-                turnOnService $debugTest apache2
-                            ;;
-            4)  echo -e "Turning off all services:\n"
-                echo -e "Apache: "
-                turnOffService $frontEnd apache2
-                echo -e "\nRabbitmq: "
-                turnOffService $message rabbitmq
-                echo -e "\nmySQL: "
-                turnOffService $database mysql
-                echo -e "\ndebug apache: "
-                turnOffService $debugTest apache2
-                            ;;
-            5)  turnOnOneService
-                            ;;
-            6)  turnOffOneService
-                            ;;
-            z)  checkActive $debugTest apache2
-                            ;;
-            *)  echo exit
-    esac
-    
+    run 
 done
