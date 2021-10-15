d4dba23db8799bca858af43288c224ae347ea3d6
JunSean-Fung <46977322+JunSean-Fung@users.noreply.github.com>
2021-10-14 21:55:24 -0400

    Update move.sh

--- milestones/milestone_3/move.sh
+++ milestones/milestone_3/move.sh
@@ -1,6 +1,4 @@
-# move front-end folder and its contents to the /var/www/html
-#sudo mv front_end /var/www/html/
+# This script move dir and files to specific location
 
-rsync -av --delete front_end /var/www/html
-rsync -av  front_end/front_end.conf /etc/apache2/sites-available/
-rsync -av  front_end/tecmint.com.conf /etc/apache2/sites-available/
+rsync -av --delete front_end /var/www/html # replace dir with new content
+rsync -av  front_end/front_end.conf /etc/apache2/sites-available/ #move the conf to dir
