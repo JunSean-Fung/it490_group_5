afe9d2e829caa2ced4fde4354658f96caefcf008
JunSean-Fung <46977322+JunSean-Fung@users.noreply.github.com>
2021-10-17 17:05:37 -0400

    Update git_ctl.sh

--- 6_milestones/milestone_3/git_ctl.sh
+++ 6_milestones/milestone_3/git_ctl.sh
@@ -4,7 +4,7 @@
 branch = "JunSean"
 
 # Back up the current repo in the local machine
-sudo rsync -av --delete it490_group_5 ~/it490_backup # replace old backup with the current local repo
+sudo rsync -av --delete ~/it490_group_5 ~/it490_backup # replace old backup with the current local repo
 
 # Reset the git
 git reset --hard
@@ -12,7 +12,7 @@ git reset --hard
 git pull origin $branch
 
 # Move replace with new contents
-sudo rsync -av --delete front_end /var/www/html #front_end
+sudo rsync -av --delete ~/it490_group_5/6_milestones/milestone_3/front_end /var/www/html #front_end
 #rsync -av  front_end/front_end.conf /etc/apache2/sites-available/ #update config file
 
 # Give execute permission to all files based on the format
