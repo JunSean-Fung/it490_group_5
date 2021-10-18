46abaa2674fa16d2e172071a3f65c9bb7b6e914e
JunSean-Fung <46977322+JunSean-Fung@users.noreply.github.com>
2021-10-18 00:51:53 -0400

    Update db_key.php

--- 6_milestones/milestone_3/front_end/db_key.php
+++ 6_milestones/milestone_3/front_end/db_key.php
@@ -4,11 +4,11 @@
     $sql_password='fall2021';
     $sql_database="simplycoding";
 
-    
     function connect_db() {
         global $sql_host, $sql_username, $sql_password, $sql_database;
         $conn=new mysqli($sql_host,$sql_username,$sql_password);
         if(mysqli_connect_error() !== null) {
+            $msg = "could not conenct to database.<br/>";
             return false;
         }
         $conn -> select_db($sql_database);
