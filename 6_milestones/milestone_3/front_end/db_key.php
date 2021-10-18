5edf27c22082d21f61640d046cfadcfe95709656
JunSean-Fung <46977322+JunSean-Fung@users.noreply.github.com>
2021-10-18 01:47:28 -0400

    Update db_key.php

--- 6_milestones/milestone_3/front_end/db_key.php
+++ 6_milestones/milestone_3/front_end/db_key.php
@@ -7,10 +7,8 @@
     function connect_db() {
         global $sql_host, $sql_username, $sql_password, $sql_database;
         
-        
         #$conn=new mysqli($sql_host,$sql_username,$sql_password,$sql_database);
-        
-        $conn=new mysqli($sql_host,$sql_database);
+        $conn=new mysqli_connect($sql_host,$sql_username,$sql_password, $sql_database);
         header('location: milestone_2_frontEnd.html');
         if(mysqli_connect_error() !== null) {
             
