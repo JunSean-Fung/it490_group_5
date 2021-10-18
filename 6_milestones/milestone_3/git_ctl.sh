2921bb3dcefc21732728e19b1dea54eb297bee3e
JunSean-Fung <46977322+JunSean-Fung@users.noreply.github.com>
2021-10-17 20:48:50 -0400

    Update git_ctl.sh

--- 6_milestones/milestone_3/git_ctl.sh
+++ 6_milestones/milestone_3/git_ctl.sh
@@ -3,6 +3,8 @@
 # Back up location: ~/it490_backup/
 #branch = "master"
 
+# Install rsync
+sudo apt-get install rsync
 # Back up the current repo in the local machine
 sudo rsync -av --delete ~/it490_group_5 ~/it490_backup # replace old backup with the current local repo
 
