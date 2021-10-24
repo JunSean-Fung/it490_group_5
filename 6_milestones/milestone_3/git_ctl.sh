303fd8d390916240d2d3ed0bbcf09f1c9b8b9e0e
JunSean-Fung <46977322+JunSean-Fung@users.noreply.github.com>
2021-10-24 16:46:45 -0400

    update

--- 6_milestones/milestone_3/git_ctl.sh
+++ 6_milestones/milestone_3/git_ctl.sh
@@ -16,7 +16,7 @@ git pull origin master
 
 # Move replace with new contents
 sudo rsync -av --delete ~/it490_group_5/6_milestones/milestone_3/front_end /var/www/html #front_end
-sudo rsync -av --delete ~/it490_group_5/1_front_end/ /var/www/html #1_front_end
+#sudo rsync -av --delete ~/it490_group_5/1_front_end/ /var/www/html #1_front_end
 #rsync -av  front_end/front_end.conf /etc/apache2/sites-available/ #update config file
 
 # Give execute permission to all files based on the format
