f737b3038d9c093448cb0e0ad462eb60b4706ab5
JunSean-Fung <46977322+JunSean-Fung@users.noreply.github.com>
2021-10-27 13:58:28 -0400

    Update ngrok.sh

--- 6_milestones/milestone_3/ngrok.sh
+++ 6_milestones/milestone_3/ngrok.sh
@@ -5,4 +5,6 @@ cd ~
 ./ngrok authtoken 205dgcskzPJs4V5fCGVwGuhr9PA_3eMNs9y43qna9DoJeLbZP
 
 # start the tunnel to port 5000
-./ngrok http 192.168.196.196:5000
+./ngrok http 192.168.196.196:5000 # SurfacePro Sean (DEB)
+#./ngrok http 192.168.196.94:5000 # Desktop Sean (DEB)
+
