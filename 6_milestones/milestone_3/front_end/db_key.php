f3076fbc0b32b382a70381a6aa5e156c758ae18f
JunSean-Fung <46977322+JunSean-Fung@users.noreply.github.com>
2021-10-28 22:37:24 -0400

    Update db_key.php

--- 6_milestones/milestone_3/front_end/db_key.php
+++ 6_milestones/milestone_3/front_end/db_key.php
@@ -7,8 +7,8 @@
     function connect_db() {
         global $sql_host, $sql_username, $sql_password, $sql_database;
         
-        #$conn=new mysqli($sql_host,$sql_username,$sql_password);
-        $conn=new mysqli_connect($sql_host,$sql_username,$sql_password); # Seems to be faster 
+        $conn=new mysqli($sql_host,$sql_username,$sql_password);
+        #$conn=new mysqli_connect($sql_host,$sql_username,$sql_password); # Seems to be faster 
         #$conn=new mysqli_connect($sql_host,$sql_username,$sql_password, $sql_database);
         #header('location: milestone_2_frontEnd.html'); //debug
         if(mysqli_connect_error() != null) {
