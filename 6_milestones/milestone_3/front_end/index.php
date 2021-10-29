cb452b42ebb7931b6a53cc1ed983441644cc5091
JunSean-Fung <46977322+JunSean-Fung@users.noreply.github.com>
2021-10-28 23:19:14 -0400

    Update index.php

--- 6_milestones/milestone_3/front_end/index.php
+++ 6_milestones/milestone_3/front_end/index.php
@@ -1,5 +1,25 @@
 <?php
     require 'db_key.php';
     #include "login.html"
-    include "register.php" # Debugging
+    #include "register.php" # Debugging
 ?>
+<body>
+<div class = 'container'>
+<div>
+<div><h1>Login to your account</h1></div>
+</div>
+<form method = 'POST' action = 'backend.php' >
+<div class="form-group">
+<label>Username : </label>
+<input class= 'form-control w-25' type="text" name="username">
+<label>Password :</label>
+<input class= 'form-control w-25' type="password" name="password" id="password" autocomplete="off">
+</div>
+<button class = 'btn btn-outline-info' type="submit" name="login" value= 'login' class="submit">Login</button>
+</form>
+<div id="footer-box">
+<p>Not a member? <a href="register.php" class="sign-up">Sign up now</a></p>
+</div>
+</div>
+</body>
+</html>
