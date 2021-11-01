8316cc6530aab34bb0ff64d39707379a6d09af3b
ZacharyVeliky <zacharymanatee@gmail.com>
2021-10-31 23:51:34 -0400

    Changed

--- 6_milestones/milestone_3/back_end/RabbitMQServer.php
+++ 6_milestones/milestone_3/back_end/RabbitMQServer.php
@@ -1,102 +1,81 @@
 <?php
+ini_set('display_errors',1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
+require_once('path.inc');
 require_once('get_host_info.inc');
 require_once('rabbitMQLib.inc');
+require_once("config.php");
 
-const DB_SERVER = 'localhost';
-const DB_USERNAME = 'root';
-const DB_PASSWORD = 'administratorpassword';
-const DB_DATABASE = 'test';
+function login($username, $password){
+    global $db;
 
-$db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+    $stmt = $db->prepare('SELECT id, username, password FROM Users WHERE username = :username LIMIT 1');
+    $stmt->bindParam(':username', $username);
+    $stmt->execute();
+    $results = $stmt->fetch(PDO::FETCH_ASSOC);
 
-function login($user, $pass, $db)
-{
-    $response = array();
-    try {
-        $stmt = $db->prepare("SELECT username, password from `Users` where username = :user LIMIT 1");
-        $params = array(":user" => $user);
-        $stmt->execute($params);
-        $result = $stmt->fetch(PDO::FETCH_ASSOC);
-        if ($result) {
-            $userpassword = $result['password'];
-            if (password_verify($pass, $userpassword)) {
-                $response["cookie"] = uniqid();
-                return True;
+    if($results){
+        $userpass = $results['password'];
+        if(password_verify($password, $userpass)){ //comparing plaintext and hash
+            $stmt->bindParam(':username', $username);
+            $stmt->execute();
+            if($results && count($results) > 0){
+                $userSes = array("name"=> $results['username'], "id"=> $results['id']);
+                return json_encode($userSes);
             }
-        } else {
-            return false;
+            return true;
+            echo "Logged in (Console)";
         }
-
-    } catch (Exception $e) {
-        echo $e->getMessage();
-        $client = new rabbitMQClient("rabbit.ini", "test");
-        $request = array();
-        $request['type'] = "Error";
-        $request['message'] = strval($e->getMessage());
-        //$response = $client->send_request($request);
-        $response = $client->publish($request);
-
-        echo "sent error" . PHP_EOL;
-        exit("Could not login");
+        else{
+            return false;
+            echo "invalid password";
+        }
     }
 }
 
-function register($username, $email, $password, $db)
-    {
+function register($username, $hash){
+    global $db;
 
-        $response = array();
-        try {
-            $stmt = $db->prepare("SELECT email from `Users` where email = :email ");
-            $params = array(":email" => $email);
-            $stmt->execute($params);
-            $result = $stmt->fetchAll();
-
-            if ($result[0]["email"] != NULL) {
-                $response["success"] = FALSE;
-                $response["msg"] = "Email already exists";
-                exit();
-            } else {
-                $response["cookie"] = uniqid();
-                $stmt = $db->prepare("INSERT INTO `Users` (username, email, password) VALUES (:username, :email, :password)");
-                $params = array(":username" => $username, ":email" => $email, ":password" => $password);
-                $stmt->execute($params);
-                $response["success"] = true;
-                return $response;
-            }
-        } catch (Exception $e) {
-            echo $e->getMessage();
-            $client = new rabbitMQClient("rabbit.ini", "test");
-            $request = array();
-            $request['type'] = "Error";
-            $request['message'] = strval($e->getMessage());
-            $response = $client->publish($request);
-            echo "sent error" . PHP_EOL;
-            exit("Could not create account");
-        }
-
-        function request_processor($request, $db)
-        {
-            echo "Received Request" . PHP_EOL;
-            echo "<pre>" . var_dump($request) . "</pre>";
-            if (!isset($request['type'])) {
-                return "Error: unsupported message type";
-            }
-            //Handle message type
-            $type = $request['type'];
-            switch ($type) {
-                case "login":
-                    return login($request['username'], $request['password'], $db);
-                case "register":
-                    return register($request['session_id']);
-            }
-            return array("return_code" => '0',
-                "message" => "Server received request and processed it");
-        }
-
-        $server = new rabbitMQServer("rabbit.ini", "test");
-
+    //checking if username exists already
+    $usncheck = $db->prepare('SELECT * FROM Users where username = :username');
+    $usncheck->bindParam(':username', $username);
+    $usncheck->execute();
+    $results = $usncheck->fetch(PDO::FETCH_ASSOC);
+    if($results && count($results) > 0){
+        echo "Username already exists";
+        return false;
+    }
+    //check passed, inserts user
+    $quest = 'INSERT INTO Users (username, password) VALUES (:username, :password)';
+    $stmt = $db->prepare($quest);
+    $stmt->bindParam(':username', $username);
+    $stmt->bindParam(':password', $hash);
+    $stmt->execute();
 }
+
+function request_processor($req){
+    echo "Received Request".PHP_EOL;
+    echo "<pre>" . var_dump($req) . "</pre>";
+    if(!isset($req['type'])){
+        return "Error: unsupported message type";
+    }
+    //Handle message type
+    $type = $req['type']; //takes message array and puts it into req[]
+    echo "\n\n";
+    echo $type;
+    switch($type){
+        case "login":
+            return loginMessage($req['username'], $req['password']);
+        case "register":
+            return registerMessage($req['username'], $req['hash']);
+    }
+
+    return array("return_code" => '0',
+        "message" => "Server received request and processed it");
+}
+
 $server = new rabbitMQServer("rabbit.ini", "test");
 
 echo "Rabbit MQ Server Start" . PHP_EOL;
