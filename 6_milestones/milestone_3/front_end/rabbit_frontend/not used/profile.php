82f5a39a3ae0939feaf268e7c8b5ff6dd608a0f6
JunSean-Fung <46977322+JunSean-Fung@users.noreply.github.com>
2021-11-01 10:13:51 -0400

    updated from Zach

---
+++ 6_milestones/milestone_3/front_end/rabbit_frontend/not used/profile.php
@@ -0,0 +1,88 @@
+<?php
+include_once("blade/header.php");
+require('/var/www/html/it490-project/rmq/RabbitMQClient.php');
+
+//Displaying movie lists
+$response = displayMovieList($id);
+$responseA = displayFavMovie($id);
+$responseB = displayReviews($id);
+
+if($response == false && $responseA == false && $responseB == false ){
+    echo "Response is false!";
+}
+$list = json_decode($response, true);
+$favList = json_decode($responseA, true);
+$reviewList = json_decode($responseB, true);
+?>
+
+<head>
+    <title>User Profile</title>
+</head>
+<div class="row rounded">
+    <div class="col">
+        <h2>Favorite Movies</h2>
+        <p>
+            Your favorite movies with a score of 9 or higher.
+        <table class="table table-sm">
+            <thead>
+            <tr>
+                <th scope="col">Title</th>
+            </tr>
+            </thead>
+            <tbody>
+            <?php foreach($favList as $index=>$row):?>
+                <tr>
+                    <?php echo "<td>".$row['movie_title']."</td>";?>
+                </tr>
+            <?php endforeach;?>
+            </tbody>
+        </table>
+        </p>
+    </div>
+    <div class="col">
+        <h2>Movie List</h2>
+        <p>
+        All movies added to your list. Go <a href="listmanager.php">here</a> to edit.
+        <table class="table table-sm">
+            <thead>
+            <tr>
+                <th scope="col">Title</th>
+                <th scope="col">Score</th>
+            </tr>
+            </thead>
+            <tbody>
+            <?php foreach($list as $index=>$row):?>
+                <tr>
+                    <?php echo "<td>".$row['movie_title']."</td>";?>
+                    <?php echo "<td>".$row['score']."</td>";?>
+                </tr>
+            <?php endforeach;?>
+            </tbody>
+        </table>
+        </p>
+    </div>
+    <div class="col">
+        <h2>Movie Reviews</h2>
+        <p>
+        <table class="table table-sm">
+            <thead>
+            <tr>
+                <th scope="col">Title</th>
+                <th scope="col">Review</th>
+            </tr>
+            </thead>
+            <tbody>
+            <?php foreach($reviewList as $index=>$row):?>
+                <tr>
+                    <?php echo "<td>".$row['movie_title']."</td>";?>
+                    <?php echo "<td>".$row['review']."</td>";?>
+                </tr>
+            <?php endforeach;?>
+            </tbody>
+        </table>
+        </p>
+    </div>
+</div>
+<?php
+include_once("blade/footer.php");
+?>
