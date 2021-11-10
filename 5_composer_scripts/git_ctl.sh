6ea77143c71331241d8e90a84932dd5896de2601
JunSean-Fung <46977322+JunSean-Fung@users.noreply.github.com>
2021-11-10 10:11:11 -0500

    added files

---
+++ 5_composer_scripts/git_ctl.sh
@@ -0,0 +1,25 @@
+# This scrpit will be used to: reset the git, pull, 
+#and replace +update dir and files.
+# Back up location: ~/it490_backup/
+#branch = "master"
+
+# Install rsync
+sudo apt-get install rsync
+# Back up the current repo in the local machine
+sudo rsync -av --delete ~/it490_group_5 ~/it490_backup # replace old backup with the current local repo
+
+# Reset the git
+git reset --hard
+# Pull the repo from branch
+git config pull.ff only
+git pull origin JunSean
+#git pull origin paul
+
+# Move replace with new contents
+sudo rsync -av --delete ~/it490_group_5/6_milestones/milestone_3/front_end /var/www/html #front_end
+sudo rsync -av --delete ~/it490_group_5/1_front_end /var/www/html #1_front_end
+#rsync -av  front_end/front_end.conf /etc/apache2/sites-available/ #update config file
+
+# Give execute permission to all files based on the format
+find -type f -iname "*.sh" -exec chmod +x {} \;
+find -type f -iname "*.py" -exec chmod +x {} \;
---
+++ 5_composer_scripts/ngrok.sh
@@ -0,0 +1,10 @@
+# Navigate to the home
+cd ~
+
+# Connect to ngrok account
+./ngrok authtoken 205dgcskzPJs4V5fCGVwGuhr9PA_3eMNs9y43qna9DoJeLbZP
+
+# start the tunnel to port 5000
+./ngrok http 192.168.196.196:5000 # SurfacePro Sean (DEB)
+#./ngrok http 192.168.196.94:5000 # Desktop Sean (DEB)
+
