479bc6464a3b15c8f04dbabd9cb8b0a63bca0cfe
JunSean-Fung <46977322+JunSean-Fung@users.noreply.github.com>
2021-11-16 22:05:16 -0500

    Update register.php

--- 6_milestones/milestone_4/front_end/register.php
+++ 6_milestones/milestone_4/front_end/register.php
@@ -1,5 +1,32 @@
 <?php
    require 'header.php';
+   require('RabbitMQClient.php');
+   if(isset($_POST['submitButton'])){
+      $username = $_POST['username'];
+      $password = $_POST['password'];
+      $confirm = $_POST['confirmPassword'];
+      if($password != $confirm)
+      {
+         echo "Passwords dont match";
+         exit();
+      }
+
+      if ($username != "" && $password != ""){
+         $hash = password_hash($password, PASSWORD_BCRYPT);
+         $rabbitResponse = register($username, $hash);
+
+         if($rabbitResponse==false){
+               echo "account already created";
+
+         }else{
+
+               echo "Account is created";
+               header("Location: login.php");
+
+         }
+      }else{
+         echo "Nothing entered";
+      }
 ?>
 <!DOCTYPE html>
 <html lang="en">
