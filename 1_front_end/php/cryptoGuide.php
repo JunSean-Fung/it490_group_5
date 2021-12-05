b9264f9a7c83b6cdc35219d9400ea22c582d0ce0
JunSean-Fung <46977322+JunSean-Fung@users.noreply.github.com>
2021-12-05 16:02:59 -0500

    update

--- 1_front_end/php/cryptoGuide.php
+++ 1_front_end/php/cryptoGuide.php
@@ -7,38 +7,51 @@
 
 <!doctype html>
 <html lang="en">
-   <head>
-        <title>Project X: Crypto</title>
-        <!-- Bootstrap/ CSS -->
-        <!-- Meta-->
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="description" content="IT490 project">
-        <meta name="author" content="Group 5">    
-        <!-- Bootstrap 5 from getbootstrap.com -->
-        <link rel="canonical" href="https://getbootstrap.com/docs/5.1/examples/album/">
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-        <link rel="canonical" href="https://getbootstrap.com/docs/5.1/examples/headers/">
-   </head>
-   <body>
-        <!--This is the topbar-->
-        <div id = "topBar">
-            <?php 
-                include('../projectX/php/topBar.php')
-            ?>
-        </div>
-        <main>
-            
-        </main>
-        <!-- Footer -->
-        <footer class="text-muted footer">
-            <div>
-            <ul>
-               <li><a href="../projectX/php/about.php">About Us</a></li>
-               <li><a href="https://github.com/JunSean-Fung/it490_group_5">Github</a></li>
-            </ul>
-            </div>
-        </footer>
+<head>
+    <title>ProjectX: Crypto</title>
+    <!-- Bootstrap/ CSS -->
+    <!-- Meta-->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="IT490 project">
+    <meta name="author" content="Group 5">    
+    <!-- Bootstrap 5 from getbootstrap.com -->
+    <link rel="canonical" href="https://getbootstrap.com/docs/5.1/examples/album/">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="canonical" href="https://getbootstrap.com/docs/5.1/examples/headers/">
+</head>
+<body>
+    <!--This is the topbar-->
+    <div id = "topBar">
+        <?php 
+            include('../projectX/php/topBar.php')
+        ?>
+    </div>
 
-   </body>
+    <!-- Teseting Table with API data -->
+    <table class="table table-bordered" style="margin-top:90px">
+        <thead>
+            <th scope="col">UUID</th>
+            <th scope="col">BTC Price</th>
+            <th scope="col">Rank</th>
+            <th scope="col">Tier</th>
+            <th scope="col">Name</th>
+            <th scope="col">Price</th>
+            <th scope="col">Symbol</th>
+        </thead>
+    </table>    
+
+    <!-- Footer -->
+    <footer class="text-muted footer">
+        <div>
+        <ul>
+            <li><a href="../projectX/php/about.php">About Us</a></li>
+            <li><a href="https://github.com/JunSean-Fung/it490_group_5">Github</a></li>
+        </ul>
+        </div>
+    </footer>
+
+    <!-- Calling the Crypto API via the JS -->
+    <script src="crypto.js"></script>
+</body>
 </html>
