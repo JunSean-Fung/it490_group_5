9ebd84009a2eb2cc90ac0a6b5beb49fb0b34d1ee
JunSean-Fung <46977322+JunSean-Fung@users.noreply.github.com>
2021-12-05 16:16:25 -0500

    fixed

--- 1_front_end/js/crypto.js
+++ 1_front_end/js/crypto.js
@@ -1,5 +1,5 @@
 var baseUrl = "https://api.coinranking.com/v2/coins"
-var proxyUrl = "https://cors-anywhere.herokuapp.com"
+var proxyUrl = "https://cors-anywhere.herokuapp.com/"
 var apiKey = "coinranking8f1fe4cf0e85fa1c2360a15f9e3ba860fb49a89b674f1022"
 
 fetch(`${proxyUrl}${baseUrl}`, {
