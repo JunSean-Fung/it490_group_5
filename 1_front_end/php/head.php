fa4e9a3ba443fc4d9a49b305bf73c59dd2c0ef0e
JunSean-Fung <46977322+JunSean-Fung@users.noreply.github.com>
2021-12-06 11:06:24 -0500

    head.php is not working for some reason, trying to fix

--- 1_front_end/php/head.php
+++ 1_front_end/php/head.php
@@ -2,17 +2,15 @@
     # Purpose: Called from all other web page to set up the header and also authenticate the correct user
     # NEEDS authentication on the specific user
 ?>
-<!doctype html>
-<html lang="en">
-    <head>
-        <!-- Meta-->
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="description" content="IT490 project">
-        <meta name="author" content="Group 5">    
-        <!-- Bootstrap 5 from getbootstrap.com -->
-        <link rel="canonical" href="https://getbootstrap.com/docs/5.1/examples/album/">
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-        <link rel="canonical" href="https://getbootstrap.com/docs/5.1/examples/headers/">
-    </head> 
-</html>
+
+<head>
+    <!-- Meta-->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="IT490 project">
+    <meta name="author" content="Group 5">    
+    <!-- Bootstrap 5 from getbootstrap.com -->
+    <link rel="canonical" href="https://getbootstrap.com/docs/5.1/examples/album/">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="canonical" href="https://getbootstrap.com/docs/5.1/examples/headers/">
+</head> 
