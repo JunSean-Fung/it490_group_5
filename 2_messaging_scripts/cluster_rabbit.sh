f0f2e255e853444da22d6a6eb298b30ef386f7c7
root <root@rabbit.fios-router.home>
2021-12-14 15:58:00 -0500

    another fix

--- 2_messaging_scripts/cluster_rabbit.sh
+++ 2_messaging_scripts/cluster_rabbit.sh
@@ -2,6 +2,7 @@ sudo systemctl stop rabbitmq-server
 echo "JFKZVCBYEISEQILVZMSD" | sudo tee /var/lib/rabbitmq/.erlang.cookie
 sudo systemctl start rabbitmq-server
 rabbitmq-plugins enable rabbitmq_management
+sudo echo 10.242.187.72 rabbit /etc/hosts
 sudo rabbitmqctl stop_app
 sudo rabbitmqctl join_cluster rabbit@rabbit
 sudo rabbitmqctl start_app
