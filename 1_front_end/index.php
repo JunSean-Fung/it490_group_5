7697ddff8559ec6d4dc642f71b25de138fcb87e7
JunSean-Fung <46977322+JunSean-Fung@users.noreply.github.com>
2021-12-18 02:26:20 -0500

    Update index.php

--- 1_front_end/index.php
+++ 1_front_end/index.php
@@ -22,6 +22,22 @@
         <link href="../projectX/css/headers.css" rel="stylesheet">
         <!-- <link href="../projectX/css/product.css" rel="stylesheet"> -->
         <link href="../projectX/css/mainPage.css" rel="stylesheet">
+
+        <style>
+         .bd-placeholder-img {
+         font-size: 1.125rem;
+         text-anchor: middle;
+         -webkit-user-select: none;
+         -moz-user-select: none;
+         user-select: none;
+         }
+
+         @media (min-width: 768px) {
+         .bd-placeholder-img-lg {
+            font-size: 3.5rem;
+         }
+         }
+      </style>
    </head>
    <body>
       
