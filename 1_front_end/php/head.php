f9353a86c5c4ce53cfb8c50e59ddb2b7e2fa19c7
JunSean-Fung <46977322+JunSean-Fung@users.noreply.github.com>
2021-12-18 00:04:31 -0500

    Update head.php

--- 1_front_end/php/head.php
+++ 1_front_end/php/head.php
@@ -9,11 +9,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="IT490 project">
     <meta name="author" content="Group 5">    
-    <!-- Bootstrap 5 from getbootstrap.com -->
-    <!-- <link rel="canonical" href="https://getbootstrap.com/docs/5.1/examples/album/">
+    <!-- Bootstrap 5 -->
+    <link rel="canonical" href="https://getbootstrap.com/docs/5.1/examples/album/">
+    
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <link rel="canonical" href="https://getbootstrap.com/docs/5.1/examples/headers/"> -->
+    
+    <!-- BT5 for Header -->
+    <link rel="canonical" href="https://getbootstrap.com/docs/5.1/examples/headers/">
 
-    <!-- Bootstrap 4 -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 </head> 
