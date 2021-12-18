4ab3af40573ea7f76240c7999f62d78d3ec43057
JunSean-Fung <46977322+JunSean-Fung@users.noreply.github.com>
2021-12-18 01:35:41 -0500

    update

--- 1_front_end/php/topBar.php
+++ 1_front_end/php/topBar.php
@@ -14,16 +14,19 @@
     */
 ?>
 <div class="container">         
+    <head>
+        <!-- Linked to topBar.css -->
+        <link href="../projectX/css/topBar.css" rel="stylesheet">
+    </head>
     <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 border-bottom">
-    <h1>Project X</h1>
-    <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">               
-        
-    </ul>
-    <!-- Login section -->
-    <div class="col-md-3 text-end">
-        <a href='../projectX/php/login.php'>
-            <button type="button" class="btn btn-outline-primary me-2">Login/Register</button>
-        </a>
-    </div>
+        <h1 id="title" >Project X</h1>
+        <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">               
+        </ul>
+        <!-- Login section -->
+        <div class="col-md-3 text-end loginBtn">
+            <a href='../projectX/php/login.php'>
+                <button type="button" class="btn btn-outline-warning me-2">Login/Register</button>
+            </a>
+        </div>
     </header>
 </div>
