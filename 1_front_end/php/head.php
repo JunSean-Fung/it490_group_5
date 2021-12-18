d95e21a099823301010f7033c2082a79d9fdc64b
JunSean-Fung <46977322+JunSean-Fung@users.noreply.github.com>
2021-12-18 00:34:57 -0500

    Update head.php

--- 1_front_end/php/head.php
+++ 1_front_end/php/head.php
@@ -14,7 +14,7 @@
    
     <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"> -->
     <!-- Backup Local Bootstrap core css -->
-    <link href="../assets/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="../projectX/assets/dist/css/bootstrap.min.css" rel="stylesheet">
 
     <!-- BT5 examples for Header -->
     <!-- <link rel="canonical" href="https://getbootstrap.com/docs/5.1/examples/headers/"> -->
