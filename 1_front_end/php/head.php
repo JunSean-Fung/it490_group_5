1831a8cf2ae4ed11e2c613179476616bd7284794
JunSean-Fung <46977322+JunSean-Fung@users.noreply.github.com>
2021-12-18 00:38:07 -0500

    Update head.php

--- 1_front_end/php/head.php
+++ 1_front_end/php/head.php
@@ -11,14 +11,12 @@
     <meta name="author" content="Group 5">    
     
     <!-- Bootstrap 5 -->
-   
-    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"> -->
-    <!-- Backup Local Bootstrap core css -->
-    <link href="../projectX/assets/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Core CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
     <!-- BT5 examples for Header -->
     <!-- <link rel="canonical" href="https://getbootstrap.com/docs/5.1/examples/headers/"> -->
-     <!-- <link rel="canonical" href="https://getbootstrap.com/docs/5.1/examples/album/"> -->
+    <!-- <link rel="canonical" href="https://getbootstrap.com/docs/5.1/examples/album/"> -->
 
     <!-- End of Bootstrap 5 -->
 </head> 
