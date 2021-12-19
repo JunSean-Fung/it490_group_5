b9a5a162235db111affece18c3e3b2b4117cb4b2
JunSean-Fung <46977322+JunSean-Fung@users.noreply.github.com>
2021-12-18 21:36:51 -0500

    Update crypto_searchBar.js

--- 1_front_end/js/crypto_searchBar.js
+++ 1_front_end/js/crypto_searchBar.js
@@ -9,7 +9,7 @@ $(document).ready(function() {
     function searchCoin(currency) {
         console.log("SearchCoin Sucess");
         var baseUrl = "https://api.coinranking.com/v2/search-suggestions?query=" + currency;
-        //var proxyUrl = "https://cors-anywhere.herokuapp.com/" // From CORS Anwhere  <-- This one works, but only 50 per 3 minutes, so becareful
+        var proxyUrl = "https://cors-anywhere.herokuapp.com/" // From CORS Anwhere  <-- This one works, but only 50 per 3 minutes, so becareful
         var apiKey = "coinranking8f1fe4cf0e85fa1c2360a15f9e3ba860fb49a89b674f1022"
 
         fetch(`${proxyUrl}${baseUrl}`, {
