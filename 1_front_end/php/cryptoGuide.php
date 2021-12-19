ecfc4a0113627ead466df593e5fecace843fc0ed
JunSean-Fung <46977322+JunSean-Fung@users.noreply.github.com>
2021-12-19 12:36:11 -0500

    Update cryptoGuide.php

--- 1_front_end/php/cryptoGuide.php
+++ 1_front_end/php/cryptoGuide.php
@@ -25,17 +25,27 @@
 <body>
     <!--This is the topbar-->
 
-    <!-- Search Bar link to API-->
-    <!-- <form action="*">
-      <input type="text" placeholder="Search for Coin" name="search" id="CoinSearchInput">
-      <button onClick="searchCoin()" ><i class="fa fa-search"></i> Search</button>
-    </form> -->
-
-    <div class="container-fluid text-center">
-        
+    <!-- Slogan with search bar -->
+    <div class="container-fluid">
+        <div class="row">
+            <div class=" Jumbotron p-3 p-md-5 d-flex align-items-center text-white bg-dark">
+                <div>
+                    <h1>Every Road to riches start with a search</h1>
+                </div>
+                <!-- Search Bar -->
+                <div class="col-md-12">
+                    <div class="search-2"> 
+                        <i class='bx bxs-map'></i> 
+                        <input type="text" placeholder="San Francisco,USA"> 
+                        <button>Search</button> 
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
     <!-- Table with search result from the API data -->
     <table class="table table-bordered" style="margin-top:20px">
-        Top Five Crypto
+        <h1>Top Five Crypto</h1>
         <thead>            
             <th scope="col">Name</th>
             <th scope="col">Symbol</th>
