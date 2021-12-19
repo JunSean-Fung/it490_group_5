eec0baaa3c98afaa9930eb69af4bec28b7e27220
JunSean-Fung <46977322+JunSean-Fung@users.noreply.github.com>
2021-12-19 12:37:39 -0500

    update

--- 1_front_end/php/cryptoGuide.php
+++ 1_front_end/php/cryptoGuide.php
@@ -26,23 +26,7 @@
     <!--This is the topbar-->
 
     <!-- Slogan with search bar -->
-    <div class="container-fluid">
-        <div class="row">
-            <div class=" Jumbotron p-3 p-md-5 d-flex align-items-center text-white bg-dark">
-                <div>
-                    <h1>Every Road to riches start with a search</h1>
-                </div>
-                <!-- Search Bar -->
-                <div class="col-md-12">
-                    <div class="search-2"> 
-                        <i class='bx bxs-map'></i> 
-                        <input type="text" placeholder="San Francisco,USA"> 
-                        <button>Search</button> 
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+    
     <!-- Table with search result from the API data -->
     <table class="table table-bordered" style="margin-top:20px">
         <h1>Top Five Crypto</h1>
