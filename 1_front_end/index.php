d9bbe74867a6c1193cf7f0335ffe40f29391d1eb
JunSean-Fung <46977322+JunSean-Fung@users.noreply.github.com>
2021-12-20 13:54:36 -0500

    Update index.php

--- 1_front_end/index.php
+++ 1_front_end/index.php
@@ -118,8 +118,8 @@
       <script src="../assets/dist/js/bootstrap.bundle.min.js"></script>
       <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
    </body>
-
-   <footer  class="py-3 my-4">
+   
+   <footer  class="container-fluid py-3 my-4">
       <div class = "container">
          <ul class="nav justify-content-center border-bottom pb-3 mb-3">
             <li class="nav-item"><a href="https://github.com/JunSean-Fung/it490_group_5" class="nav-link px-2 text-muted">Github</a></li>
