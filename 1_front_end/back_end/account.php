6186ba5df26013628a9b230afd5136d4324fbe9e
JunSean-Fung <46977322+JunSean-Fung@users.noreply.github.com>
2021-12-20 16:14:40 -0500

    testing loging

---
+++ 1_front_end/back_end/account.php
@@ -0,0 +1,17 @@
+<?php
+    # 
+    session_start(); # Start session
+    
+    if( !isset( $_SESSION['username']) ) # if username is not found in database
+    {
+        #echo "You are not authorized to view this page. Go back <a href= '/'>home</a>";
+        echo "This user is not found. Try again <a href= '/front_end/login.html'>Login</a>";
+        
+        exit();
+    }
+    # Calling other pages
+    #require 'header.php' # call the bootstrap styling
+    header('location: ../front_end/mainpage_logged.html'); // Head to logged in main page
+    #require 'mainpage.html'
+    
+?>
