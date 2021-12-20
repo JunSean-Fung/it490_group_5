4173388cfc0e31d37fb2df66ecd82dc0d48c76c5
JunSean-Fung <46977322+JunSean-Fung@users.noreply.github.com>
2021-12-19 23:29:12 -0500

    Update index.php

--- 1_front_end/index.php
+++ 1_front_end/index.php
@@ -81,14 +81,14 @@
                         <div class="col-lg-6 order-lg-2 text-white showcase-img" style="background-image: url('../projectX/assets/img/img_1.jpg')"></div>
                         <div class="col-lg-6 order-lg-1 my-auto showcase-text">
                               <h2>Find any Games Reviews</h2>
-                              <p class="lead mb-0">Details/ descption about this</p>
+                              <p class="lead mb-0">You can see the top trending games and find any games review that you want to see in just 3 clicks</p>
                         </div>
                      </div>
                      <div class="row g-0">
                         <div class="col-lg-6 text-white showcase-img" style="background-image: url('../projectX/assets/img/img_2.jpg')"></div>
                         <div class="col-lg-6 my-auto showcase-text">
                               <h2>View Most Up-to-date info about Crypto</h2>
-                              <p class="lead mb-0">Details/ descption about this</p>
+                              <p class="lead mb-0">You can see the top trending games and find any games review that you want to see in just 3 clicks</p>
                         </div>
                      </div>                
                   </div>
