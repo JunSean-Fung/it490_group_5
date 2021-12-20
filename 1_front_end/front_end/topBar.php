afd2e0a42c02ed446398c7293c552c6ccfe7aac0
JunSean-Fung <46977322+JunSean-Fung@users.noreply.github.com>
2021-12-20 18:06:29 -0500

    added the old front_end

---
+++ 1_front_end/front_end/topBar.php
@@ -0,0 +1,21 @@
+<?php 
+    /* 
+    This file will be called everytime a page is loaded 
+    and will prompt to authenticate user session/ profile 
+    */
+    
+?>
+<div class="container">         
+    <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 border-bottom">
+    <h1>Project X</h1>
+    <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">               
+        
+    </ul>
+    <!-- Login section -->
+    <div class="col-md-3 text-end">
+        <a href='login.php'>
+            <button type="button" class="btn btn-outline-primary me-2">Login/Register</button>
+        </a>
+    </div>
+    </header>
+</div>
