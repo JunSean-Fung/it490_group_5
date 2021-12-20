baa7a5d87d3114fa3ab92171b094a15e4aa9336b
JunSean-Fung <46977322+JunSean-Fung@users.noreply.github.com>
2021-12-20 15:42:53 -0500

    trying to fix login

--- 1_front_end/php/login.php
+++ 1_front_end/php/login.php
@@ -1,52 +1,45 @@
 <?php
-    /* Purpose: 
-        (1) Allow user to login an account
-        (2) Allow user to sign up an account
+// require '../back_end/header.php';
 
-        Note:
-        1) [Path]: Since this is in the php folder, path is this: ../folderName/fileName.fileType
+session_start();
+require('../php/RabbitMQClient.php');
 
-        Task:
-        1) Refine the responsiveness with bootstrap
-        2) Additional interactions such as: go back to the main page, reset password? , 
-    */
-    # Get header file
-    require '../php/head.php'; # Needs to be "../php/head.php"
-    session_start();
-    require '../php/RabbitMQClient.php'; #BM!!!! SOMETHING IS BROKEN AGAIN
-
-    if (isset($_POST['login'])) {
-        try {
-            $username = $_POST['username'];
-            $password = $_POST['password'];
-            if ($username != "" && $password != "") {
-                $rabbitResponse = login($username, $password);
-                if ($rabbitResponse == false) {
-                    echo "login has failed, please try again";
-                    //redirect back to login page to try again
-                } else {
-                    echo "You are logged in!";
-                    $userSes            = json_decode($rabbitResponse, true);
-                    $_SESSION['logged'] = true;
-                    $_SESSION['user']   = $userSes;
-                    echo var_export($_SESSION['user']['name']);
-                    header("location: ../html/mainpage_logged.html"); 
-                }
+if (isset($_POST['login'])) {
+    try {
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+        if ($username != "" && $password != "") {
+            $rabbitResponse = login($username, $password);
+            if ($rabbitResponse == false) {
+                echo "login has failed, please try again";
+                //redirect back to login page to try again
             } else {
-                echo "username and password is empty";
+                echo "You are logged in!";
+                $userSes            = json_decode($rabbitResponse, true);
+                $_SESSION['logged'] = true;
+                $_SESSION['user']   = $userSes;
+                echo var_export($_SESSION['user']['name']);
+                header("location: ../projectX/index.php");
             }
-        }
-        catch (Exception $e) {
-            echo $e->getMessage();
+        } else {
+            echo "username and password is empty";
         }
     }
+    catch (Exception $e) {
+        echo $e->getMessage();
+    }
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
     <head>
-        
         <title>Project X: Login</title>
-        <link rel="stylesheet" href="../css/loginNReg.css"/>
+        <!-- <link rel="stylesheet" href="../css/loginNReg.css"/> -->
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+        <!-- <link rel="stylesheet" href="../front_end/css/styles.css"/> -->
     </head>
     <body>
         <div class="container">
