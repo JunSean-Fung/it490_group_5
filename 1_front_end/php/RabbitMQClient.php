baf28a47e975832854457b7ac1f9f339e1fe04c3
JunSean-Fung <46977322+JunSean-Fung@users.noreply.github.com>
2021-12-20 20:17:35 -0500

    update

--- 1_front_end/php/RabbitMQClient.php
+++ 1_front_end/php/RabbitMQClient.php
@@ -3,14 +3,15 @@ require_once('path.inc');
 require_once('get_host_info.inc');
 require_once('rabbitMQLib.inc');
 
-function register($username, $hash){
+function register($username, $password){
     $client = new RabbitMQClient('rabbit.ini', 'it490Server');
     if(isset($argv[1])){
     }
     else{
-        $msg = array("message"=>"Register", "type"=>"register", "username" => $username, "hash" => $hash ); //added hash. server listens for "register" now for registering
+        $msg = array("message"=>"Register", "type"=>"register", "username" => $username, "password" => $password ); //added hash. server listens for "register" now for registering
 
     }
+
 
 
     $response = $client->send_request($msg);
