7661cc348c3264a51207d354a465273f5232d44d
JunSean-Fung <46977322+JunSean-Fung@users.noreply.github.com>
2021-12-20 19:47:41 -0500

    Update about.php

--- 1_front_end/php/about.php
+++ 1_front_end/php/about.php
@@ -39,73 +39,77 @@
     margin: 0;
   }
 
-html {
-  box-sizing: border-box;
-}
-
-*, *:before, *:after {
-  box-sizing: inherit;
-}
-
-.column {
-  float: left;
-  width: 33.3%;
-  margin-bottom: 16px;
-  padding: 0 8px;
-}
-
-.card {
-  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
-  max-width: 500px;
-  
-  margin: auto;
-  text-align: center;
-}
-
-.about-section {
-  padding: 50px;
-  text-align: center;
-  background-color: #474e5d;
-  color: white;
-}
-
-.container {
-  padding: 0 16px;
-}
-
-.container::after, .row::after {
-  content: "";
-  clear: both;
-  display: table;
-}
-
-.title {
-  color: grey;
-}
-
-.button {
-  border: none;
-  outline: 0;
-  display: inline-block;
-  padding: 8px;
-  color: white;
-  background-color: #000;
-  text-align: center;
-  cursor: pointer;
-  width: 100%;
-}
-
-.button:hover {
-  background-color: #555;
-}
-
-@media screen and (max-width: 650px) {
-  .column {
-    width: 100%;
-    display: block;
+  html {
+    box-sizing: border-box;
   }
-}
-    </style>
+
+  *, *:before, *:after {
+    box-sizing: inherit;
+  }
+
+  .column {
+    float: left;
+    width: 33.3%;
+    margin-bottom: 16px;
+    padding: 0 8px;
+  }
+
+  .card {
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+    max-width: 500px;
+    
+    margin: auto;
+    text-align: center;
+  }
+
+  .about-section {
+    padding: 50px;
+    text-align: center;
+    background-color: #474e5d;
+    color: white;
+  }
+
+  .container {
+    padding: 0 16px;
+  }
+
+  .container::after, .row::after {
+    content: "";
+    clear: both;
+    display: table;
+  }
+
+  .title {
+    color: grey;
+  }
+
+  .button {
+    border: none;
+    outline: 0;
+    display: inline-block;
+    padding: 8px;
+    color: white;
+    background-color: #000;
+    text-align: center;
+    cursor: pointer;
+    width: 100%;
+  }
+
+  .button:hover {
+    background-color: #555;
+  }
+
+  @media screen and (max-width: 650px) {
+    .column {
+      width: 100%;
+      display: block;
+    }
+  }
+  /* New added*/
+  .aboutDes{
+    margin: 2rem;
+  }
+</style>
 
     
   </head>
@@ -153,7 +157,7 @@ html {
     <div class="about-section">
       <div class="container-fluid">
         <h1>About Us</h1>
-        <p class="mx-3">A little bit about us. We are 5 seniors attending NJIT with a passion for technology. We worked together as a group to 
+        <p class="aboutDes">A little bit about us. We are 5 seniors attending NJIT with a passion for technology. We worked together as a group to 
           create Project X to showcase our ability to create a multi server cluster with basic networkig, communication, database and security cluster.
           We show our knowledge by creating this website for gameing and crypto lovers. 
         </p>
