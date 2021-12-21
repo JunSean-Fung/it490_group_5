2143934333126f4245b3c7f75fa8860be84b8a44
JunSean-Fung <46977322+JunSean-Fung@users.noreply.github.com>
2021-12-20 21:48:46 -0500

    added error value indicator

--- 1_front_end/php/login.php
+++ 1_front_end/php/login.php
@@ -42,6 +42,15 @@
             echo $e->getMessage();
         }
     }
+    // Show error value
+    function console_log($output, $with_script_tags = true) {
+        $js_code = 'console.log(' . json_encode($output, JSON_HEX_TAG) .');';
+        if ($with_script_tags) {
+            $js_code = '<script>' . $js_code . '</script>';
+        }
+    echo $js_code;
+ }
+ console_log($rabbitResponse);
 ?>
 <!DOCTYPE html>
 <html lang="en">
