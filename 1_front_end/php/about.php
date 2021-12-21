ee910ad7cb615b1e643ea3ad2dcdf3ab93874330
JunSean-Fung <46977322+JunSean-Fung@users.noreply.github.com>
2021-12-20 19:45:21 -0500

    Update about.php

--- 1_front_end/php/about.php
+++ 1_front_end/php/about.php
@@ -151,9 +151,9 @@ html {
   
 <main> 
     <div class="about-section">
-      <div class="container-fluid px-2">
+      <div class="container-fluid">
         <h1>About Us</h1>
-        <p>A little bit about us. We are 5 seniors attending NJIT with a passion for technology. We worked together as a group to 
+        <p class="mx-3">A little bit about us. We are 5 seniors attending NJIT with a passion for technology. We worked together as a group to 
           create Project X to showcase our ability to create a multi server cluster with basic networkig, communication, database and security cluster.
           We show our knowledge by creating this website for gameing and crypto lovers. 
         </p>
