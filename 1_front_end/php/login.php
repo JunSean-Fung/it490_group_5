11180fbe50af806843e6170b1f952a4ba57be789
JunSean-Fung <46977322+JunSean-Fung@users.noreply.github.com>
2021-12-20 21:32:57 -0500

    Update login.php

--- 1_front_end/php/login.php
+++ 1_front_end/php/login.php
@@ -64,7 +64,11 @@
                             
                             <!--Login Button-->
                             <input type="submit" value="Login" name="login">
-                            
+                            <?php 
+                                if (isset($wrongPasswdMsg)){
+                                    echo "Wrong password"
+                                }
+                            ?>
                             <a class="forgot text-muted" href="#">Forgot password?</a>
                             <br>
                             <a href="../php/register.php">Dont have an account? Click here to register one!</a>
