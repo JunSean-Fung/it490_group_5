47aeb9cd7da6f01100c3c39dc16d75d93ebe4467
JunSean-Fung <46977322+JunSean-Fung@users.noreply.github.com>
2021-12-20 19:58:06 -0500

    Update about.php

--- 1_front_end/php/about.php
+++ 1_front_end/php/about.php
@@ -158,9 +158,8 @@
     <div class="about-section">
       <div class="container-fluid">
         <div class="row">
-          <div class="col">
-            <h1>About Us</h1>
-          </div>
+          <h1>About Us</h1>
+          
           <div class="col"> 
             <p class="aboutDes">A little bit about us. We are 5 seniors attending NJIT with a passion for technology. We worked together as a group to 
             create Project X to showcase our ability to create a multi server cluster with basic networkig, communication, database and security cluster.
